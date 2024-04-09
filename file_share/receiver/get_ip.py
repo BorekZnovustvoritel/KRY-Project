8dafd55b117eeac05063fcc4d2d28bd02e5031e5
BeziCZ <bezecnym@seznam.cz>
2024-04-09 17:12:14 +0200

    First docs addition and comment addition to code

--- file_share/receiver/get_ip.py
+++ file_share/receiver/get_ip.py
@@ -14,6 +14,7 @@ def get_local_ip() -> str:
 
 
 def get_broadcast_addr() -> str:
+    """Return the broadcast address for this station's subnet."""
     split_ip = get_local_ip().split(".")[0:-1]
     ip = ".".join(split_ip) + ".255"
     return ip
